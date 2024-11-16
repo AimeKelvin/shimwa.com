4c313c66ee7bfe5474139d75b6efa247292738f9
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-11-16 10:16:40 +0200

    contact_page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Twitter, Instagram } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Instagram size={20} />,
+		href: "https://twitter.com/shimmwa",
+		label: "Instagram",
+		handle: "@shimmwa",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:dev@kellykelvinaime.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "kellykelvinaime@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/AimeKelvin",
 		label: "Github",
-		handle: "chronark",
+		handle: "AimeKelvin",
 	},
 ];
 
